2b829faf8ff84a2b2737ea8e06adac30beea5993
weichinweng <weichinweng@google.com>
2020-07-28 16:56:33 +0800

    update bluetooth.cc - fix use of undeclared identifier 'is_ atv'
    update bluetooth.cc - fix use of undeclared identifier 'is_ atv' build error.

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -135,7 +135,7 @@ static bool is_profile(const char* p1, const char* p2) {
  ****************************************************************************/
 
 static int init(bt_callbacks_t* callbacks, bool start_restricted,
-                bool is_niap_mode) {
+                bool is_niap_mode, bool is_atv) {
   LOG_INFO(LOG_TAG, "%s: start restricted = %d ; niap = %d", __func__,
            start_restricted, is_niap_mode);
 
