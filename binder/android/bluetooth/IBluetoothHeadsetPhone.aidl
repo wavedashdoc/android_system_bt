938ecca3ccdea68cd183abc26667cb9e2dfc4a75
wavedashdoc <31520009+wavedashdoc@users.noreply.github.com>
2021-02-06 13:26:47 -0500

    Revert "Merge commit 'cddade6482806444d57c36cb426d7dd160d91687' into ten_ls3"
    This reverts commit 73d58bc544361a9d32a92eff456b0b117450eb75, reversing
    changes made to 91a2242e9175d682b87650743e78c3ab3459af62.

--- binder/android/bluetooth/IBluetoothHeadsetPhone.aidl
+++ binder/android/bluetooth/IBluetoothHeadsetPhone.aidl
@@ -31,8 +31,6 @@ interface IBluetoothHeadsetPhone {
   String getSubscriberNumber();
   boolean listCurrentCalls();
   boolean queryPhoneState();
-  boolean isHighDefCallInProgress();
-
 
   // Internal for phone app to call
   void updateBtHandsfreeAfterRadioTechnologyChange();
--- btif/src/btif_ble_scanner.cc
+++ btif/src/btif_ble_scanner.cc
@@ -285,11 +285,10 @@ class BleScannerInterfaceImpl : public BleScannerInterface {
                                  jni_thread_wrapper(FROM_HERE, std::move(cb))));
   }
 
-  void SetScanParameters(int scan_phy, std::vector<uint32_t> scan_interval,
-                         std::vector<uint32_t> scan_window,
+  void SetScanParameters(int scan_interval, int scan_window,
                          Callback cb) override {
     do_in_main_thread(
-        FROM_HERE, base::Bind(&BTM_BleSetScanParams, scan_interval[0], scan_window[0],
+        FROM_HERE, base::Bind(&BTM_BleSetScanParams, scan_interval, scan_window,
                               BTM_BLE_SCAN_MODE_ACTI,
                               jni_thread_wrapper(FROM_HERE, std::move(cb))));
   }
