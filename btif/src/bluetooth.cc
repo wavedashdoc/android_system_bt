3840c16958c5b0a8ea944c99b25f30a7cd8f465b
wavedashdoc <31520009+wavedashdoc@users.noreply.github.com>
2021-02-06 13:26:52 -0500

    Revert "Merge commit '343c9de3c4879ea2345ccb0b42a6b40afd44743e' into ten_ls3"
    This reverts commit 9130ffa1317fe92bd6c7d4e8d28d4f2a4ccfb2ee, reversing
    changes made to bcf025bd86067b34c3ba04c292f5c6ec69fd2a1d.

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -135,7 +135,7 @@ static bool is_profile(const char* p1, const char* p2) {
  ****************************************************************************/
 
 static int init(bt_callbacks_t* callbacks, bool start_restricted,
-                bool is_niap_mode, bool is_atv) {
+                bool is_niap_mode) {
   LOG_INFO(LOG_TAG, "%s: start restricted = %d ; niap = %d", __func__,
            start_restricted, is_niap_mode);
 
@@ -149,7 +149,8 @@ static int init(bt_callbacks_t* callbacks, bool start_restricted,
   restricted_mode = start_restricted;
   niap_mode = is_niap_mode;
   
-  is_local_device_atv = is_atv;
+  is_local_device_atv = is_atv; }
+
   stack_manager_get_interface()->init_stack();
   btif_debug_init();
   return BT_STATUS_SUCCESS;
